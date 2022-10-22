2016f3f91c01991d0667dd7aba471d3068851484
AbduHosein <hoseina@bc.edu>
2022-10-22 18:39:31 -0400

    Adds the incomplete stop routine and more TODO's

--- client/client.go
+++ client/client.go
@@ -24,9 +24,10 @@ func main() {
 		return
 	}
 	// Store args in local variables
-	// TODO: Potentialy send the username over to server to be a key in our serveside Connections map that will store the various connections.
 	CONNECT := arguments[1]
 	USERNAME := arguments[2]
+
+	// TODO: Potentialy send the username over to server to be a key in our serveside Connections map that will store the various connections.
 
 	// Printed this just to see the args
 	fmt.Println(CONNECT, USERNAME)
@@ -53,7 +54,7 @@ func main() {
 
 		// Exit client when the user types STOP.
 		// TODO (STOP GO ROUTINE FROM SERVER): Add another condition here when you recieve STOP from the server.
-		if strings.TrimSpace(string(text)) == "STOP" {
+		if strings.TrimSpace(string(text)) == "EXIT" {
 			fmt.Println("TCP client exiting...")
 			return
 		}
@@ -61,17 +62,16 @@ func main() {
 		// Here the raw text string is split into a slice, so that I can store the slice values in the message struct.
 		send := strings.Split(text, " ")
 
-		// Store the message content in a slice, so users can now send longer messages.
-		content := send[2:]
-
 		// ensure there is more than 2 inputs, ie, the client is sending a message.
 		if len(send) > 2 {
+			// Store the message content in a slice, so users can now send longer messages.
+			content := send[2:]
+
 			// Message struct created, and sent to server using encoder.
 			message := &Message{send[0], send[1], content}
 			encoder.Encode(message)
 		} else {
-			fmt.Println("Invalid arguments")
+			fmt.Println("Invalid arguments, please input: To From Message")
 		}
-
 	}
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/gob"
 	"fmt"
 	"net"
@@ -10,42 +11,59 @@ import (
 
 // TODO: A MAP that can store all the connected clients. Was thinking something like {USERNAME: netConn}
 // This way when the server recieves a message Struct, we check to see if its in the map, and handle the error if its not.
-// If it is in the MAP, we could now construct a new Struct of the form Message{From, Content string}, ex--> {"Abdu: ", "Hello"}
+// If it is in the MAP, we could now construct a new Struct of the form Message{From, Content string}, ex--> {"Abdu: ", "Hello"}\
+
+// Message struct to receive structs from the clients.
 type Message struct {
 	To      string
 	From    string
 	Content []string
 }
 
-// TODO: have a channel that can recieve values from the main() server block.
-// Was thinking we could have a channel to recieve the "STOP" signal, and a channel to recieve the Message struct
-func handleConnection(c net.Conn) {
+// Global variable used to turn the server off
+var serverStatus = "ON"
+
+// TODO: When EXIT is received, need to communicate via a channel to all the active handleconnection routines.
+// stopChatroom readers user input from Stdin and updates the serverStatus global variable when the EXIT msg is inputted.
+func stopChatroom(ch chan string) {
+	for {
+		fmt.Print(">> ")
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if strings.TrimSpace(string(text)) == "EXIT" {
+			fmt.Println("Server is shutting down... Press Ctrl + C to exit")
+			serverStatus = "OFF"
+			return
+		}
+	}
+}
+
+// TODO: When the ^^ stopChatroom goroutine receives the EXIT signal:
+//   - Need to somehow communicate to all handleConnections go routines via a channel that the chatroom is exitting,
+//   - Send the EXIT signal to the respective client via TCP.
+func handleConnection(c net.Conn, ch chan string) {
 	// decoder used to recieve messages from the respective client.
 	dec := gob.NewDecoder(c)
 
-	// encoder to send messages to the respective client.
-	//enc := gob.NewEncoder(c)
-
 	for {
 		//Read data from connection
-		fmt.Print(">> ")
-
-		//temp := strings.TrimSpace(string(netData)) from earlier code I took from concurrentTCP linode article.
 		message := &Message{}
 		dec.Decode(message)
 		temp := message.To
 
-		// I think here the only condition that matters is "" cause if the client exits, thats what is stored in the temp variable.
-		if temp == "STOP" || temp == "" {
-			fmt.Print("Client has exited...\n>> ")
+		//
+		if temp == "" {
+			fmt.Print("Client has exited...\n")
 			break
 		}
 
 		// For testing purposes only, wont need this later on...
-		// Printing the To and From values as is... and joining the Content value into one string, seperated by " ".
 		// NOTE: message.Content is a slice containing the message values in each index.
-		fmt.Print("{To field, From field, content}: " + message.To + " " + message.From + " " + strings.Join(message.Content, " "))
-		//fmt.Print(message.Content[1])
+		fmt.Print(">> {To field, From field, content}: " + message.To + " " + message.From + " " + strings.Join(message.Content, " "))
 	}
 	c.Close()
 }
@@ -68,16 +86,23 @@ func main() {
 	}
 	defer l.Close()
 
-	// This block handles incoming connections
-	// TODO (STOP GO ROUTINE FROM SERVER): launch a stopChatroom go routine that will read from Stdin from server-side.
-	// If it reads "STOP", send the "STOP" string to all client threads.
-	for {
+	// Launch the thread that will read Stdin from user on server side and update serverStatus global variable.
+	// TODO: implement the signal channel and pass to stopChatroom, and somehow use this channel to communication with all handleConnection routines.
+	signal := make(chan string)
+	go stopChatroom(signal)
+
+	// This block handles incoming connections while serverstatus is ON.
+	// TODO: Ensure that the program terminates when serverStatus is OFF, ie, make sure all handleConnection routines exit.
+	for serverStatus == "ON" {
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		fmt.Println("Server has connected to a new client...")
-		go handleConnection(c)
+
+		// TODO: make sure that these routines exit when signal feeds the "EXIT" string
+		go handleConnection(c, signal)
+
 	}
 }
