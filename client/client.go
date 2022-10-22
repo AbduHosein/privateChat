8f5cbda32e7e7962cfbd8e88af57753b48c8d076
AbduHosein <hoseina@bc.edu>
2022-10-22 14:52:48 -0400

    Adds comments and lists TODO's (tbd)

--- client/client.go
+++ client/client.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 )
 
+// Message struct to relay to the server
 type Message struct {
 	To      string
 	From    string
@@ -16,45 +17,57 @@ type Message struct {
 }
 
 func main() {
+	// Validate that client is supplying correct args.
 	arguments := os.Args
 	if len(arguments) != 3 {
 		fmt.Println("Please provide host:port USERNAME")
 		return
 	}
-
+	// Store args in local variables
+	// TODO: Potentialy send the username over to server to be a key in our serveside Connections map that will store the various connections.
 	CONNECT := arguments[1]
 	USERNAME := arguments[2]
+
+	// Printed this just to see the args
 	fmt.Println(CONNECT, USERNAME)
+
+	// Connect to the server, DIAL is only used once.
 	c, err := net.Dial("tcp", CONNECT)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Following block of code reads client Stdin, formats it, then sends to the server using Gob.
+	encoder := gob.NewEncoder(c)
 	for {
-		encoder := gob.NewEncoder(c)
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print(">> ")
+
+		// Raw input from server, unformatted at this point
 		text, _ := reader.ReadString('\n')
+
+		// Exit client when the user types STOP.
+		// TODO (STOP GO ROUTINE FROM SERVER): Add another condition here when you recieve STOP from the server.
 		if strings.TrimSpace(string(text)) == "STOP" {
 			fmt.Println("TCP client exiting...")
 			return
 		}
-		// Sending message to the server
-		//fmt.Fprintf(c, text+"\n")
+
+		// Here the raw text string is split into a slice, so that I can store the slice values in the message struct.
 		send := strings.Split(text, " ")
-		//content := send[2:len(send)]
-		message := &Message{send[0], send[1], send[2]}
-		encoder.Encode(message)
 
-		// Read response from the server.
-		//message, _ := bufio.NewReader(c).ReadString('\n')
+		// TODO: the message content in the struct should be a slice that contains all the message contents.
+		//content := send[2:] Experimenting here to pass over the message contents as a string, for messages longer than 1 word.
 
-		// Print the server's response
-		//fmt.Print("->: " + message)
-		if strings.TrimSpace(string(text)) == "STOP" {
-			fmt.Println("TCP client exiting...")
-			return
+		// For now ensure that the message is len 3: To From Content
+		if len(send) == 3 {
+			// Message struct created, and sent to server using encoder.
+			message := &Message{send[0], send[1], send[2]}
+			encoder.Encode(message)
+		} else {
+			fmt.Println("Invalid arguments")
 		}
+
 	}
 }
--- server/server.go
+++ server/server.go
@@ -7,47 +7,54 @@ import (
 	"os"
 )
 
-var count = 0
-
+// TODO: A MAP that can store all the connected clients. Was thinking something like {USERNAME: netConn}
+// This way when the server recieves a message Struct, we check to see if its in the map, and handle the error if its not.
+// If it is in the struct, we could now construct a new Struct of the form Message{From, Content string}, ex--> {Abdu:, Hello}
 type Message struct {
 	To      string
 	From    string
 	Content string
 }
 
+// TODO: have a channel that can recieve values from the main() server block.
+// Was thinking we could have a channel to recieve the "STOP" signal, and a channel to recieve the Message struct
 func handleConnection(c net.Conn) {
+	// decoder used to recieve messages from the respective client.
+	dec := gob.NewDecoder(c)
+
+	// encoder to send messages to the respective client.
+	//enc := gob.NewEncoder(c)
 
 	for {
 		//Read data from connection
 		fmt.Print(">> ")
-		dec := gob.NewDecoder(c)
-		//netData, err := bufio.NewReader(c).ReadString('\n')
-		//if err != nil {
-		//	fmt.Println(err)
-		//	return
-		//}
 
-		//temp := strings.TrimSpace(string(netData))
+		//temp := strings.TrimSpace(string(netData)) from earlier code I took from concurrentTCP linode article.
 		message := &Message{}
 		dec.Decode(message)
 		temp := message.To
+
+		// I think here the only condition that matters is "" cause if the client exits, thats what is stored in the temp variable.
 		if temp == "STOP" || temp == "" {
-			fmt.Print("Client has exited...\n")
+			fmt.Print("Client has exited...\n>> ")
 			break
 		}
+
+		// For testing purposes only, wont need this later on..
 		fmt.Print("{To field, From field, content}: " + message.To + " " + message.From + " " + message.Content)
-		//counter := strconv.Itoa(count) + "\n"
-		//c.Write([]byte("Encoded struct received on serverW"))
 	}
 	c.Close()
 }
 
 func main() {
+	// Handle errors for cmd line arguments
 	arguments := os.Args
 	if len(arguments) == 1 {
 		fmt.Println("Please provide a port number!")
 		return
 	}
+
+	// Server starts here
 	fmt.Println("Server has started...")
 	PORT := ":" + arguments[1]
 	l, err := net.Listen("tcp4", PORT)
@@ -57,6 +64,9 @@ func main() {
 	}
 	defer l.Close()
 
+	// This block handles incoming connections
+	// TODO (STOP GO ROUTINE FROM SERVER): launch a stopChatroom go routine that will read from Stdin from server-side.
+	// If it reads "STOP", send the "STOP" string to all client threads.
 	for {
 		c, err := l.Accept()
 		if err != nil {
